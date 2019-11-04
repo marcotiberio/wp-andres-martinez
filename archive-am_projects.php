e7e2f265f824a1fbbb4ef2dfb0012bc605505e01
Marco Tiberio <tiberiomrc@gmail.com>
2019-11-04 18:28:30 +0100

    clear filter

--- archive-am_projects.php
+++ archive-am_projects.php
@@ -7,8 +7,8 @@ get_header(); ?>
                 <tr>
                     <th id="date">Date</th>
                     <th id="name">Name <span class="arrowup hide">&uarr;</span><span class="arrowdown">&darr;</span></th>
-                    <th id="type">Type <span class="arrowup hide">&uarr;</span><span class="arrowdown">&darr;</span></th>
-                    <th id="clear">Clear Filter</th>
+                    <th id="type">Type <span id="clear">Clear Filter</span></th>
+                    <!-- <th id="clear">Clear Filter</th> -->
                 </tr>
             </thead>
             <?php 
