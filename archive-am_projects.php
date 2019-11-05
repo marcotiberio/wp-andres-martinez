570d8a1f8c4886d717fdbb769478080be9d002c7
Marco Tiberio <tiberiomrc@gmail.com>
2019-11-05 12:39:57 +0100

    test

--- archive-am_projects.php
+++ archive-am_projects.php
@@ -17,8 +17,11 @@ get_header(); ?>
                 <tr>
                     <th id="date">Date</th>
                     <th id="name">Name <span class="arrowup hide">&uarr;</span><span class="arrowdown">&darr;</span></th>
-                    <th id="type">Type <span class="arrowup hide">&uarr;</span><span class="arrowdown">&darr;</span></th>
-                    <div id="clear">Back to all</div>
+                    <th id="type">Type 
+                        <span class="arrowup hide">&uarr;</span><span class="arrowdown">&darr;</span>
+                        <div id="clear">Back to all</div>
+                    </th>
+                    <!-- <div id="clear">Back to all</div> -->
                 </tr>
             </thead>
             <?php 
