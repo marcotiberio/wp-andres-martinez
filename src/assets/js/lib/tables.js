852ae2a21e45ee1fcd0683235f4a766d487d84ef
Marco Tiberio <tiberiomrc@gmail.com>
2019-11-05 16:25:46 +0100

    update mobile filter

--- src/assets/js/lib/tables.js
+++ src/assets/js/lib/tables.js
@@ -2,22 +2,26 @@ $('#date').on('click',function(){
     $('#date .arrowdown').toggleClass('hide');
     $('#date .arrowup').toggleClass('hide');
 });
+
 $('#type').on('click',function(){
     $('#type .arrowdown').toggleClass('hide');
     $('#type .arrowup').toggleClass('hide');
     $('#date .arrowdown').removeClass('hide');
     $('#date .arrowup').addClass('hide');
 });
+
 $("#name").on("click", function() {                 //NEW ARROWDOWN-UP
     $("#name .arrowdown").toggleClass("hide"),      //NEW ARROWDOWN-UP
     $("#name .arrowup").toggleClass("hide"),        //NEW ARROWDOWN-UP
 
     $(".description").removeClass("activated")      //NEW ARROWDOWN-UP
-}),
+});
+
 $('#wdate').on('click',function(){
     $('#wdate .arrowdown').toggleClass('hide');
     $('#wdate .arrowup').toggleClass('hide');
 });
+
 $("#wname").on("click", function() {                 //NEW ARROWDOWN-UP
     $("#wname .arrowdown").toggleClass("hide"),      //NEW ARROWDOWN-UP
     $("#wname .arrowup").toggleClass("hide"),        //NEW ARROWDOWN-UP
@@ -25,13 +29,15 @@ $("#wname").on("click", function() {                 //NEW ARROWDOWN-UP
     $("#wdate .arrowup").addClass("hide"),           //NEW ARROWDOWN-UP
 
     $(".description").removeClass("activated")       //NEW ARROWDOWN-UP
-}),
+});
+
 // $('#wtype').on('click',function(){
 //     $('#wtype .arrowdown').toggleClass('hide');
 //     $('#wtype .arrowup').toggleClass('hide');
 //     $('#wdate .arrowdown').toggleClass('hide');
 //     $('#wdate .arrowup').toggleClass('hide');
 // });
+
 $("#namedisc span").click(function() {
       $(this).parent().children(".description").toggleClass("activated");
     });
