4a6d57d2afb47f0d43b679e32b03fbbeb8b4a858
Marco Tiberio <tiberiomrc@gmail.com>
2019-11-05 10:35:42 +0100

    arrowdownup

--- src/assets/js/lib/tables.js
+++ src/assets/js/lib/tables.js
@@ -31,8 +31,8 @@ $("#wname").on("click", function() {                 //NEW ARROWDOWN-UP
 $('#wtype').on('click',function(){
     $('#wtype .arrowdown').toggleClass('hide');
     $('#wtype .arrowup').toggleClass('hide');
-    $('#wdate .arrowdown').removeClass('hide');
-    $('#wdate .arrowup').addClass('hide');
+    $('#wdate .arrowdown').toggleClass('hide');
+    $('#wdate .arrowup').toggleClass('hide');
 });
 $("#namedisc span").click(function() {
       $(this).parent().children(".description").toggleClass("activated");
