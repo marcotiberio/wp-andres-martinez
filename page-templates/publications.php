b5ac57fa5cbf6ba12a72d7bed07c29a3c6f2326a
Marco Tiberio <tiberiomrc@gmail.com>
2019-11-05 12:53:53 +0100

    update

--- page-templates/publications.php
+++ page-templates/publications.php
@@ -22,7 +22,7 @@ get_header(); ?>
                     <th id="date">Date <span class="arrowup hide">&uarr;</span><span class="arrowdown">&darr;</span></th>
                     <th id="name">Name <span class="arrowup hide">&uarr;</span><span class="arrowdown">&darr;</span></th>
                     <th id="type">Type</th>
-                    <th id="clear">Back to Filter</th>
+                    <th id="clear">Back to all</th>
                 </tr>
             </thead>
             <?php if ( have_rows('publications') ): ?>
