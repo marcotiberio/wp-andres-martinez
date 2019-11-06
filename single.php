7c97d3b2f9d5d72b7fed72e582e4ddf6e2381069
Marco Tiberio <tiberiomrc@gmail.com>
2019-11-06 17:09:19 +0100

    update

--- single.php
+++ single.php
@@ -11,7 +11,6 @@ get_header(); ?>
 <?php get_template_part( 'template-parts/featured-image' ); ?>
 <div class="main-container">
 	<div class="main-grid">
-		ciao
 		<main class="main-content">
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php get_template_part( 'template-parts/content', '' ); ?>
