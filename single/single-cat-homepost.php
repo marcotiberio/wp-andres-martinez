c2e2c5348e2a62e805805297f464b049b51cd1af
Marco Tiberio <tiberiomrc@gmail.com>
2019-11-07 14:50:53 +0100

    update

--- single/single-cat-homepost.php
+++ single/single-cat-homepost.php
@@ -15,11 +15,7 @@ ciao
 		<main class="main-content">
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php get_template_part( 'template-parts/content', '' ); ?>
-				<?php the_post_navigation(); ?>
-				<?php comments_template(); ?>
 			<?php endwhile; ?>
 		</main>
-		<?php get_sidebar(); ?>
 	</div>
 </div>
-<?php get_footer();
