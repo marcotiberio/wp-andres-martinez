f6798d68a6fc4e0bdf3ae1afe33279628573116f
Marco Tiberio <tiberiomrc@gmail.com>
2019-11-07 15:04:03 +0100

    update single post

--- functions.php
+++ functions.php
@@ -108,33 +108,33 @@ register_taxonomy(
 add_action( 'init', 'initialize_taxonomy', 0 );
 
 
-/*
-* Define a constant path to our single template folder
-*/
-define(SINGLE_PATH, TEMPLATEPATH . '/single');
+// /*
+// * Define a constant path to our single template folder
+// */
+// define(SINGLE_PATH, TEMPLATEPATH . '/single');
  
-/**
-* Filter the single_template with our custom function
-*/
-add_filter('single_template', 'my_single_template');
+// /**
+// * Filter the single_template with our custom function
+// */
+// add_filter('single_template', 'my_single_template');
  
-/**
-* Single template function which will choose our template
-*/
-function my_single_template($single) {
-global $wp_query, $post;
+// /**
+// * Single template function which will choose our template
+// */
+// function my_single_template($single) {
+// global $wp_query, $post;
  
-/**
-* Checks for single template by category
-* Check by category slug and ID
-*/
-foreach((array)get_the_category() as $cat) :
+// /**
+// * Checks for single template by category
+// * Check by category slug and ID
+// */
+// foreach((array)get_the_category() as $cat) :
  
-if(file_exists(SINGLE_PATH . '/single-cat-' . $cat->slug . '.php'))
-return SINGLE_PATH . '/single-cat-' . $cat->slug . '.php';
+// if(file_exists(SINGLE_PATH . '/single-cat-' . $cat->slug . '.php'))
+// return SINGLE_PATH . '/single-cat-' . $cat->slug . '.php';
  
-elseif(file_exists(SINGLE_PATH . '/single-cat-' . $cat->term_id . '.php'))
-return SINGLE_PATH . '/single-cat-' . $cat->term_id . '.php';
+// elseif(file_exists(SINGLE_PATH . '/single-cat-' . $cat->term_id . '.php'))
+// return SINGLE_PATH . '/single-cat-' . $cat->term_id . '.php';
  
-endforeach;
-}
+// endforeach;
+// }
