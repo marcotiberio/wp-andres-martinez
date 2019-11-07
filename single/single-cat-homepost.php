a4b0ac0b05489b1dbc3f1786fb64fc097f74f7a9
Marco Tiberio <tiberiomrc@gmail.com>
2019-11-07 15:10:25 +0100

    update no sidebar etc

--- single/single-cat-homepost.php
+++ single/single-cat-homepost.php
@@ -14,11 +14,11 @@ get_header(); ?>
 		<main class="main-content">
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php get_template_part( 'template-parts/homepage-post', '' ); ?>
-				<?php the_post_navigation(); ?>
-				<?php comments_template(); ?>
+				
+				
 			<?php endwhile; ?>
 		</main>
-		<?php get_sidebar(); ?>
+		
 	</div>
 </div>
-<?php get_footer();
+
