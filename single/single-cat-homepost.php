20bf02190b7dec764cdc084efe4fd5de4aee01bf
Marco Tiberio <tiberiomrc@gmail.com>
2019-11-11 16:48:45 +0100

    test

--- single/single-cat-homepost.php
+++ single/single-cat-homepost.php
@@ -8,14 +8,13 @@
 
 get_header(); ?>
 
-<div class=""><?php get_template_part( 'template-parts/featured-image' ); ?></div>
+<?php get_template_part( 'template-parts/featured-image' ); ?>
 <div class="main-container">
 	<div class="main-grid">
 		<main class="main-content">
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php get_template_part( 'template-parts/homepage-post', '' ); ?>
 				<?php the_post_navigation(); ?>
-				<?php comments_template(); ?>
 			<?php endwhile; ?>
 		</main>
 		<?php get_sidebar(); ?>
