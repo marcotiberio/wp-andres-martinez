0948ada8888a86f80c13558be23dc1211dc77658
Marco Tiberio <tiberiomrc@gmail.com>
2019-11-20 11:50:08 +0100

    fixed imprint

--- page-templates/about.php
+++ page-templates/about.php
@@ -40,7 +40,6 @@ $imprint = get_field('imprint');
         <div class="imprint">
             <span>Imprint</span>
             <div class="imprint-text hide">
-            <!-- &copy; <?php echo (date("Y")." ".substr(get_bloginfo( 'name' ), 0, -1)); ?> -->
             <?php echo $imprint; ?>
             </div>
         </div>
