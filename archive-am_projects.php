e74d1130c2eef5298b52b82844e9d77dc4572f56
Marco Tiberio <tiberiomrc@gmail.com>
2019-11-20 15:15:15 +0100

    order work

--- archive-am_projects.php
+++ archive-am_projects.php
@@ -27,7 +27,7 @@ get_header(); ?>
                 'post_type' => 'am_projects',
                 'posts_per_page' => -1,
                 'meta_key' => 'year',
-                'orderby' => 'meta_value',
+                'orderby' => 'meta_value_num',
                 'order'	=> 'DESC'
             );
             $works = new WP_Query( $args ); ?>
