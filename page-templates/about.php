14310882b4a9ce7a46f60b30f93e9df10c34074e
Marco Tiberio <tiberiomrc@gmail.com>
2019-11-20 11:49:36 +0100

    fix imprint

--- page-templates/about.php
+++ page-templates/about.php
@@ -40,7 +40,7 @@ $imprint = get_field('imprint');
         <div class="imprint">
             <span>Imprint</span>
             <div class="imprint-text hide">
-            &copy; <?php echo (date("Y")." ".substr(get_bloginfo( 'name' ), 0, -1)); ?>
+            <!-- &copy; <?php echo (date("Y")." ".substr(get_bloginfo( 'name' ), 0, -1)); ?> -->
             <?php echo $imprint; ?>
             </div>
         </div>
