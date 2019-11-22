9c6486a4c640d7255253ca8fdda1df5371186bb2
Marco Tiberio <tiberiomrc@gmail.com>
2019-11-22 11:37:48 +0100

    x

--- src/assets/js/lib/activation.js
+++ src/assets/js/lib/activation.js
@@ -20,15 +20,3 @@ $(document).ready(function(){
         }
     });
 });
-
-// LOADER
-
-jQuery(document).ready(function() {
-    /* Show the HTML page only after the js and css are completely loaded */
-    delayShow();
-  });
-  
-  function delayShow() {
-    var secs = 1000;
-    setTimeout('jQuery("body").css("visibility","visible");', secs);
-  }
