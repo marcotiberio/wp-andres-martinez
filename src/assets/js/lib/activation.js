c5f95e9868ac9d2465428141598fdd42de9c2be9
Marco Tiberio <tiberiomrc@gmail.com>
2019-11-22 11:29:08 +0100

    test

--- src/assets/js/lib/activation.js
+++ src/assets/js/lib/activation.js
@@ -14,22 +14,9 @@ $(document).ready(function(){
 
 $(document).ready(function(){
     $("#work").tablesorter({
-        sortList : [0,1],
         initialized: function(){
             $('#work').animate({opacity: 'toggle'}, {duration: 400, start: function() {
                 $(this).css('display', 'table');
-            }
-        });
-        }
-    });
-});
-
-$(document).ready(function(){
-    $("#news").tablesorter({
-        sortList : [0,1],
-        initialized: function(){
-            $('#news').animate({opacity: 'toggle'}, {duration: 400, start: function() {
-                $(this).css('display', 'block');
             }
         });
         }
