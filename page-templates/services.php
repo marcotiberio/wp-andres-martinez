b47aa70f56f8fddf09644644d0d1d7a277e6cf83
Marco Tiberio <tiberiomrc@gmail.com>
2019-11-26 16:59:18 +0100

    teste loader

--- page-templates/services.php
+++ page-templates/services.php
@@ -9,6 +9,8 @@ $desc = get_field('description');
 $rightcolumn = get_field('rightcolumn');
 ?>
 
+<div class="loader"></div>
+
 <div class="grid-x grid-padding-x about">
     <div class="cell medium-2 small-12 photograph">
         <img src="<?php echo $photo[url]; ?>" alt="<?php echo $photo[alt]; ?>">
