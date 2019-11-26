3ddf4ed2c80336e5fb4d6f7f72d2fecb86c201a5
Marco Tiberio <tiberiomrc@gmail.com>
2019-11-26 13:19:48 +0100

    test loader

--- src/assets/js/lib/activation.js
+++ src/assets/js/lib/activation.js
@@ -1,3 +1,10 @@
+// LOADER
+
+jQuery(document).ready(function() {
+    /* Show the HTML page only after the js and css are completely loaded */
+    delayShow();
+  });
+
 $(document).ready(function(){ 
     $("#publications").tablesorter({
         initialized: function(){
