7d4673ac3df8033849955b0ef1976cb8d2712872
Marco Tiberio <tiberiomrc@gmail.com>
2019-11-26 11:45:40 +0100

    fading

--- src/assets/js/lib/activation.js
+++ src/assets/js/lib/activation.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){ 
     $("#publications").tablesorter({
         initialized: function(){
-            $('#publications').animate({opacity: 'toggle'}, {duration: 900, start: function() {
+            $('#publications').animate({opacity: 'toggle'}, {duration: 500, start: function() {
                 $(this).css('display', 'table');
             }
         });
@@ -13,10 +13,14 @@ $(document).ready(function(){
     $("#work").tablesorter({
         sortList : [0,1],
         initialized: function(){
-            $('#work').animate({opacity: 'toggle'}, {duration: 900, start: function() {
+            $('#work').animate({opacity: 'toggle'}, {duration: 500, start: function() {
                 $(this).css('display', 'table');
             }
         });
         }
     });
+});
+
+$(document).ready(function(){
+    setTimeout(function() {$('body').fadeIn(250); }, 500);
 });
